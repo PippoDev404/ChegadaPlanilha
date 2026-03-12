b560aa027c0dd314df976d4f57f01477351cbb36
Filippo Di Giuseppe <pippodigiuseppe19@gmail.com>
2026-03-12 13:51:12 -0300

    Teste da versão do Mini App

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ChegadaPlanilha',
+
   css: {
     preprocessorOptions: {
       scss: {
@@ -13,25 +15,32 @@ export default defineConfig({
       },
     },
   },
+
   plugins: [
-    // Allows using React dev server along with building a React application with Vite.
-    // https://npmjs.com/package/@vitejs/plugin-react-swc
     react(),
-    // Allows using the compilerOptions.paths property in tsconfig.json.
-    // https://www.npmjs.com/package/vite-tsconfig-paths
+
+    legacy({
+      targets: [
+        'defaults',
+        'Android >= 7',
+        'Chrome >= 70',
+        'iOS >= 12'
+      ]
+    }),
+
     tsconfigPaths(),
-    // Creates a custom SSL certificate valid for the local machine.
-    // Using this plugin requires admin rights on the first dev-mode launch.
-    // https://www.npmjs.com/package/vite-plugin-mkcert
-    process.env.HTTPS && mkcert(),
-  ],
+
+    process.env.HTTPS ? mkcert() : undefined,
+  ].filter(Boolean),
+
   build: {
-    target: 'esnext',
+    target: 'es2017',
     minify: 'terser'
   },
+
   publicDir: './public',
+
   server: {
-    // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
   },
-});
+})
